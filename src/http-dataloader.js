05779679e1c31b662227cd37efb1369d1afe6244
ttokutake <tadatoshi.tokutake@gmail.com>
2018-12-22 21:35:23 +0900

    Add type checks

--- src/http-dataloader.js
+++ src/http-dataloader.js
@@ -1,21 +1,31 @@
 const DataLoader = require('dataloader');
 require('isomorphic-fetch');
 
-async function request(url, options = {}) {
+async function request(url, options) {
   const { responseType, parseText } = options;
-  const resp = await fetch(url, options);
-  switch (responseType) {
-    case 'text': {
-      return resp.text();
+  if (responseType === 'custom' && typeof parseText !== 'function') {
+    throw new TypeError('"parseText" must be Function when "responseType" is "custom"');
+  }
+  try {
+    const resp = await fetch(url, options);
+    if (resp.status >= 400) {
+      throw new URIError(`HTTP response's status is ${resp.status}, body is "${await resp.text()}"`);
     }
-    case 'custom': {
-      const respBody = await resp.text();
-      return parseText(respBody);
+    switch (responseType) {
+      case 'text': {
+        return resp.text();
+      }
+      case 'custom': {
+        const respBody = await resp.text();
+        return parseText(respBody);
+      }
+      case 'json':
+      default: {
+        return resp.json();
+      }
     }
-    case 'json':
-    default: {
-      return resp.json();
-    }
+  } catch (err) {
+    throw err;
   }
 }
 
@@ -24,14 +34,19 @@ class HttpDataLoader {
     this.data = {};
   }
 
-  set({key, url, options}) {
-    if (this.data[key]) {
-      return;
+  set(...params) {
+    for (let i = 0, { length } = params; i < length; i++) {
+      const { key, url, options } = params[i];
+      if (typeof key !== 'string') {
+        throw new TypeError('"key" must be String');
+      }
+      if (typeof url !== 'string') {
+        throw new TypeError('"url" must be String');
+      }
+      if (!(typeof options === 'object' || options === undefined)) {
+        throw new TypeError('"options" must be Object, Null or Undefined');
+      }
     }
-    this.data[key] = new DataLoader(async () => [await request(url, options)]);
-  }
-
-  setAll(...params) {
     const ps = params.filter(({ key }) => !this.data[key]);
     const paramsMap = {};
     ps.forEach(param => {
@@ -39,15 +54,31 @@ class HttpDataLoader {
     });
     const dataLoader = new DataLoader(keys => Promise.all(keys.map(key => {
       const { url, options } = paramsMap[key];
-      return request(url, options);
+      return request(url, options || {});
     })));
     ps.forEach(({ key }) => {
       this.data[key] = dataLoader;
     });
   }
 
-  load(key) {
-    return this.data[key].load(key);
+  async load(...keys) {
+    if (!keys.length) {
+      throw new TypeError('Argument must be set');
+    }
+    for (let i = 0, { length } = keys; i < length; i++) {
+      const key = keys[i];
+      if (typeof key !== 'string') {
+        throw new TypeError('"key" must be String');
+      }
+      if (!(this.data[key] instanceof DataLoader)) {
+        throw new ReferenceError(`"url" whose "key" is "${key}" is not set`);
+      }
+    }
+    const result = await Promise.all(keys.map(key => this.data[key].load(key)));
+    if (result.length === 1) {
+      return result[0];
+    }
+    return result;
   }
 }
 
