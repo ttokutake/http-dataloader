68413e7554527e4cf22167acb55abcd5229dbd70
ttokutake <tadatoshi.tokutake@gmail.com>
2018-12-24 18:04:02 +0900

    Use mock for fetch()

--- test/http-dataloader.test.ts
+++ test/http-dataloader.test.ts
@@ -1,13 +1,51 @@
 import HttpDataLoader = require("../dist/http-dataloader");
 
+interface Global {
+  fetch?: any;
+}
+
+declare const global: Global;
+
 describe("HttpDataLoader", () => {
-  test("ok", async () => {
-    HttpDataLoader.set({
-      key: "package",
-      url:
-        "https://raw.githubusercontent.com/ttokutake/http-dataloader/master/package.json"
+  const origin: Global = {};
+
+  beforeEach(() => {
+    origin.fetch = global.fetch;
+    global.fetch = jest.fn(async () => {
+      return {
+        json: async () => ({ name: "http-dataloader", version: "1.0.0" }),
+        text: async () => "1.0.0"
+      };
     });
-    const result = await HttpDataLoader.load("package");
-    expect(result.name).toBe("http-dataloader");
+
+    HttpDataLoader.set(
+      {
+        key: "config.json",
+        url: "https://example.com/config.json"
+      }
+      // {
+      //   key: "version.txt",
+      //   responseType: "text",
+      //   url: "https://example.com/version.txt"
+      // }
+    );
   });
+
+  afterEach(() => {
+    global.fetch = origin.fetch;
+  });
+
+  test("responseType: json", async () => {
+    const result = await HttpDataLoader.load("config.json");
+    expect(result).toEqual({ name: "http-dataloader", version: "1.0.0" });
+    expect(global.fetch.mock.calls.length).toBe(1);
+    expect(global.fetch.mock.calls[0][0]).toBe(
+      "https://example.com/config.json"
+    );
+  });
+
+  // test("responseType: text", async () => {
+  //   const result = await HttpDataLoader.load("version.txt");
+  //   expect(result).toBe("1.0.0");
+  // });
 });
